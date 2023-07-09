5c9bddad8fa1c7c9dea6421252b65d5f8b3ac7e6
Cal Symmonds <99576651+calebSymmonds@users.noreply.github.com>
2023-07-09 14:35:48 -0700

    cleaned up main.py

--- main.py
+++ main.py
@@ -15,10 +15,7 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
                                     prompt="What's another state?\nType 'EXIT' to quit.").title()
     if answer_state == "Exit":
-        missing_states = []
-        for state in states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in states if state not in guessed_states]
         new_data = pandas.DataFrame(missing_states)
         new_data.csv("states_missed.csv")
         break
